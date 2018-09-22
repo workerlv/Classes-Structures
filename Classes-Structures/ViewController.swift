e1731b335629f2ccbc2158c8ef3f4e04e52b9c6c
Workerlv <arturs280@inbox.lv>
2018-09-22 16:11:05 +0300

    Classes and Structures example

--- Classes-Structures/ViewController.swift
+++ Classes-Structures/ViewController.swift
@@ -12,14 +12,58 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        exampleClasses()
+
+        exampleStructures()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func exampleClasses () {
+        
+        print("\n ---Example classes starts--- \n")
+        
+        let macBook = MacBooks(year: 2017, color: "Grey")
+        
+        print("macBook color = \(macBook.color)")
+        
+        let changeMacBook = macBook
+        changeMacBook.color = "Black"
+        
+        print("changeMacBook color = \(changeMacBook.color)")
+        print("macBook color = \(macBook.color)")
+        print("\n ---Example classes ends--- \n")
     }
+    
+    func exampleStructures () {
+        
+        print("\n ---Example structures starts--- \n")
+        
+        let iPhone = iPhones(number: 7, color: "Black")
+        
+        print("iPhone color = \(iPhone.color)")
 
+        var changeIPhone = iPhone
+        changeIPhone.color = "Yellow"
+        
+        print("changeIPhone color = \(changeIPhone.color)")
+        print("iPhone color = \(iPhone.color)")
+        print("\n ---Example structures ends--- \n")
 
+    }
 }
 
+class MacBooks {
+    var year: Int
+    var color: String
+    
+    init(year: Int, color: String) {
+        self.year = year
+        self.color = color
+    }
+}
+
+struct iPhones {
+    var number: Int
+    var color: String
+}
